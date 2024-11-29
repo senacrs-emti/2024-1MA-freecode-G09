000d8ea71b0de5e01d8841a07eba636bf65e8145
Pedro Henrique Tavares de Andrade <60176035079@senacrs.edu.br>
2024-11-29 12:08:34 -0300

    Atualização

--- interface.js
+++ interface.js
@@ -1,1 +1,11 @@
-document.getElementById
+void Setup() {
+  Serial.begin(9600); // Inicia a comunicação serial na velocidade de 9600 bps
+}
+
+void loop() {
+    if (Serial.available()) { // Verifica se há dados recebidos
+        String comando = Serial.readString(); // Lê os dados recebidos
+        Serial.println("Recebido: " + comando); // Envia uma resposta
+ }
+ delay(100); // Pequena pausa
+}
